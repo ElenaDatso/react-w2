aa2f43edc740fabcbd69ad6b4fa00bdcc0e1c07e
Olena Datso <elena.shcheglova@gmail.com>
2023-04-15 12:55:34 -0600

    feat: add redux provider

--- w2/src/App.tsx
+++ w2/src/App.tsx
@@ -1,6 +1,8 @@
 import './theme/global.scss';
 import { RouterProvider } from 'react-router-dom';
 import router from './routing/Router';
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return <RouterProvider router={router} />;
